65a310df8df7b1471bd7fb9fefb36feba6fda1ce
Aminub <Image de profil  Amine Naboulsi naboulsiamine10062001@gmail.com>
2023-03-02 22:31:34 +0100

    same edit

--- MiniProject/src/main/java/com/example/servlets/servlet1.java
+++ MiniProject/src/main/java/com/example/servlets/servlet1.java
@@ -1,5 +1,6 @@
 package com.example.servlets;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,30 +13,66 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
 
+@WebServlet("servlet1")
 public class servlet1 extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username =request.getParameter("username");
+		String password =request.getParameter("password");
+		request.setAttribute("username",username);
+		request.setAttribute("password",password);
+		
+		RequestDispatcher dispater = null;
+		boolean find =false;
 			try {
-				Connection con = connection_mysql.getConnection();
-				if(con != null){
-//					PreparedStatement preparedStatement =con.prepareStatement("select * from user");
-//					ResultSet rc = preparedStatement.executeQuery();
-//					while(rc.next())
-//					{
-//						System.out.println(rc.getString("name"));
-//					}
-//					PreparedStatement pre =con.prepareStatement("insert into user values (? ,? ,?)");
-//					pre.setString(1, "a");
-//					pre.setString(2, "a@gmail.com");
-//					pre.setString(3, "a");
-//					pre.executeUpdate();pre.close();
-//					con.close();
-					
+				if(request.getAttribute("username").equals("")
+						||request.getAttribute("username").equals("")) {
+					  response.getWriter().println("<script type=\"text/javascript\">");
+					  response.getWriter().println("alert('fill information first');");
+					  response.getWriter().println("location='login.jsp';");
+					  response.getWriter().println("</script>");
+					return;
 				}else {
-					System.out.println("khawi");
+					Connection con = connection_mysql.getConnection();
+					if(con != null){
+						PreparedStatement preparedStatement =
+								con.prepareStatement("select * from user");
+						dispater =request.getRequestDispatcher("Home_page.jsp");
+						ResultSet rc = preparedStatement.executeQuery();
+						while(rc.next()) {
+							if ( (rc.getString("name").equals(""+request.getAttribute("username")) 
+									|| rc.getString("email").equals(""+request.getAttribute("username")) )
+									&& rc.getString("password").equals(""+request.getAttribute("password")) ) {
+									find = true;
+									System.out.print("login successfuly for "+rc.getString("name")+" identified by "+rc.getString("password"));
+									request.setAttribute("status","success");
+									System.out.println("</br>success");
+									dispater.forward(request, response);
+							}
+						}
+						if(!find) {
+							System.out.print("login faild");
+							PrintWriter out = response.getWriter();
+							 	out.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.4/sweetalert2.all.js'></script>");
+			                   out.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>");
+			                   out.println("<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js'></script>");
+			                   out.println("<script language=JavaScript>");
+			                   out.println("$(document).ready(function(){");
+			                   out.println("swal('welcome','successful!','success');");
+			                   out.println("});");
+			                   out.println("</script>");
+							request.getRequestDispatcher("login.jsp").forward(request, response);
+						}
+						
+						
+					}else {
+						System.out.println("khawi");
+					}
 				}
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
--- MiniProject/src/main/webapp/login.jsp
+++ MiniProject/src/main/webapp/login.jsp
@@ -4,16 +4,14 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Sign Up Form by Colorlib</title>
-
-<!-- Font Icon -->
+<title>Sign Up Form </title>
 <link rel="stylesheet"
 	href="fonts/material-icon/css/material-design-iconic-font.min.css">
 
-<!-- Main css -->
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
 	<div class="main">
 
 		<section class="sign-in">
@@ -29,19 +27,17 @@
 
 					<div class="signin-form">
 						<h2 class="form-title">Sign up</h2>
-						
-						
-						<form method="get" action="login_page" class="register-form" id="login-form">
+						<form method="get" action="servlet1" class="register-form" id="login-form">
 							<div class="form-group">
 								<label for="username"><i
-									class="zmdi zmdi-account material-icons-name"></i></label><input
+									class="zmdi zmdi-account material-icons-name"></i></label> <input
 									type="text" name="username" id="username"
-									placeholder="Entrer votre email" />
+									placeholder="Your Name" />
 							</div>
 							<div class="form-group">
 								<label for="password"><i class="zmdi zmdi-lock"></i></label> <input
 									type="password" name="password" id="password"
-									placeholder="Entrer votre mot de passe" />
+									placeholder="Password" />
 							</div>
 							<div class="form-group">
 								<input type="checkbox" name="remember-me" id="remember-me"
@@ -53,9 +49,6 @@
 									class="form-submit" value="Log in" />
 							</div>
 						</form>
-						
-						
-						
 						<div class="social-login">
 							<span class="social-label">Or login with</span>
 							<ul class="socials">
@@ -79,5 +72,6 @@
 	<script src="vendor/jquery/jquery.min.js"></script>
 	<script src="js/main.js"></script>
 </body>
-<!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+
 </html>
